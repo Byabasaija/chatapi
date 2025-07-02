116c06944a1769a9ca2f4deaf87b54cc2449dc84
Byabasaija <basaijapascal9@gmail.com>
2025-07-02 21:47:47 +0300

    🔥 rm: unnecessary files

--- app/api/api_v1/web_sockets.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.api_v1.routes import websocket
-
-# Create the main API router
-web_sockets_router = APIRouter()
-
-web_sockets_router.include_router(websocket.router)
